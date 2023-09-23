8f352a1e90d4cbc1368651e92e43a6dd0050a759
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-23 15:43:12 +0900

    feat: generate scanner URL
    Co-authored-by: Coffiey <coffiey@users.noreply.github.com>

---
+++ app/backend/controller/tokenController.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+require("dotenv").config({ path: "../.env" });
+
+
+const getOneTimeToken = async (req, res) => {
+    const API_KEY = process.env.API_KEY;
+    const ORG_ID = process.env.ORG_ID;
+    const fetchUrl = `https://platform.bodygram.com/api/orgs/${ORG_ID}/scan-tokens`;
+    const body = {
+        "scope": [
+            "api.platform.bodygram.com/scans:create",
+        ]
+    };
+    const headers = {
+        'Content-Type': 'application/json', 
+        'Authorization': `${API_KEY}`,
+
+      };
+      try {
+          const results = await axios.post(fetchUrl, body, {headers});
+          const URL = `https://platform.bodygram.com/en/${ORG_ID}/scan?token=${results.data.token}&system=metric`
+     console.log(results.data);
+        res.status(200).send(URL);
+    } catch (err) {
+        res.status(500).json("something went wrong");
+    }
+}
+
+module.exports = { getOneTimeToken }
--- app/backend/index.js
+++ app/backend/index.js
@@ -1,13 +1,39 @@
-require("dotenv").config({ path: "./.env.local" });
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const API_KEY = process.env.API_KEY;
+const ORG_ID = process.env.ORG_ID;
+const tokenController = require("./controller/tokenController")
+
 
 // app.use("/", express.static(__dirname + "/.." + "/build"));
 
 app.get("/", (req, res) => {
   res.send("Hello World! test");
 });
+
+app.get("/api/getToken", tokenController.getOneTimeToken)
+
+
+
+app.post("/send-scan-data", (req, res) => {
+  const data = req.body.data;
+  const url = `https://platform.bodygram.com/api/orgs/${process.env.ORG_ID}/scans`;
+
+  const headers = {
+    'Authorization': process.env.API_KEY,
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
+  })
+  .then(() => {
+    
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
