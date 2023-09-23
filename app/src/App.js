4599ed0904b318605e8117b33fe6e6ff22dca1b9
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-23 15:01:17 +0900

    style: App and Button components

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Button from './components/Button';
-import Image from './components/Image';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div classname="main">
-        <Image />
+      <div className="main">
+        <img src="/ergonomic-desk-setup.png" alt="human figure sitting at desk with chair"></img>
         <Button 
           className="scan__button"
           title="Click to scan your body!"
--- app/src/components/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Image = () => {
-  return (
-    <></>
-  )
-}
-
-export default Image;
