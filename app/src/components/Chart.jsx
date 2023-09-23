36502415cff561e4c9557c9fc0f85425c7325a41
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-23 17:41:55 +0900

    feat: add measurement chart

---
+++ app/src/components/Chart.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Chart.css";
+
+const Chart = () => {
+  return (
+    <>
+      <table className="dimensions">
+        <tr>
+          <th colspan="2">Dimensions</th>
+        </tr>
+        <tr>
+          <td>A</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>B</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>C</td>
+          <td>3</td>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default Chart;
